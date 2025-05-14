a37f4614e71aa5c1baa22e4e1ea8a7315ff8e591
nababorah-crx <nborah@claritasrx.com>
2025-05-14 18:41:16 +0530

    Fastapi base folder strcture added

---
+++ frameworks/fastapi/app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from app.core.config import settings
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    docs_url=f"{settings.API_V1_STR}/docs",
+)
+
+# Set all CORS enabled origins
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
