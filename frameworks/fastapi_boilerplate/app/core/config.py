6d6a12535b1bc671ff9abb3ef806af8d2b7348fa
Priyanshu Arora <priyanshu@gkmit.co>
2025-05-26 16:34:20 +0530

    docker setup for fastapi

--- frameworks/fastapi_boilerplate/app/core/config.py
+++ frameworks/fastapi_boilerplate/app/core/config.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):  # type: ignore[misc]
     """Settings for the application."""
 
     API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "Test Project"
+    BACKEND_CORS_ORIGINS: list[str] =  ["http://localhost:4200"]
 
 
 settings: Settings = Settings()
